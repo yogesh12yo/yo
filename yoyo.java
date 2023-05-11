c270471139ed07adeb535ba72e63572e33e33254
yogesh12yo <133174338+yogesh12yo@users.noreply.github.com>
2023-05-10 17:28:23 -0700

    Create yoyo.java

---
+++ yoyo.java
@@ -0,0 +1,15 @@
+package hello;
+
+public class yoyo{
+
+	public static void main(String[] args) {
+		
+		
+		int a=10;
+		for (int i=1;i<=10;i++)
+		{
+			System.out.println(a+"*"+i+"="+i*a);
+		}
+	}
+
+}
